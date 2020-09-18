6f0b4af01d814682c7bb543f64a2386389e0aa05
eliott veyrier <eliottarloveyrier@live.fr>
2020-09-18 21:28:16 +0200

    Save trail script

---
+++ trail.gd
@@ -0,0 +1,29 @@
+extends Line2D
+
+export var step_size = 5
+export var max_points = 20
+export var kill_every = 0.02
+
+var last_point = Vector2()
+var target
+
+var timer = 0
+func _ready():
+	set_as_toplevel(true)
+	target = get_parent()
+	last_point = target.global_position
+
+func _physics_process(delta):
+	var new_point = target.global_position
+	print(new_point)
+	if ((new_point - last_point).length_squared() >= pow(step_size,2)):
+		add_point(new_point)
+	if get_point_count() > max_points:
+		remove_point(0)
+	timer += delta
+	if timer > kill_every:
+		remove_point(0)
+		timer = 0
+
+	
+	
